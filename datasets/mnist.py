77d183fd03dd525717b0878f292a343b22683631
Xzh0u <littleland.sxz@gmail.com>
2020-09-07 14:36:23 +0800

    feat: modify code and make it run

--- datasets/mnist.py
+++ datasets/mnist.py
@@ -11,9 +11,10 @@ def get_mnist(train):
     """Get MNIST dataset loader."""
     # image pre-processing
     pre_process = transforms.Compose([transforms.ToTensor(),
-                                      transforms.Normalize(
-                                          mean=params.dataset_mean,
-                                          std=params.dataset_std)])
+                                      #   transforms.Normalize(
+                                      #       mean=params.dataset_mean,
+                                      #       std=params.dataset_std)])
+                                      transforms.Normalize([0.5], [0.5])])
 
     # dataset and data loader
     mnist_dataset = datasets.MNIST(root=params.data_root,
